ec78864b2020d10782f35539cf7300253a296d94
Andrews Medina <andrewsmedina@gmail.com>
2016-06-16 13:28:35 -0300

    add crawler example

---
+++ crawler1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func getPage(address string) (string, error) {
+	log.Printf("logading %s", address)
+	resp, err := http.Get(address)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	log.Printf("%s loaded", address)
+	return string(body), err
+}
+
+func main() {
+	pages := []string{"http://tsuru.io", "http://globo.com", "http://g1.globo.com"}
+	for _, page := range pages {
+		_, err := getPage(page)
+		if err != nil {
+			log.Printf("Error getting %s content", page)
+		}
+	}
+}
